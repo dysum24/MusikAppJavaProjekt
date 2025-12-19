163ba50a9085420fff5e0a0c1aba0fe61c862d50
dylancele <dylansumner8403@gmail.com>
2025-12-19 11:17:43 +0100

    teste

--- src/main/java/com/example/javaprojektmusikapp/HelloApplication.java
+++ src/main/java/com/example/javaprojektmusikapp/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     {
         // TEST
         //Testaaaaaaaa
-        //Dylan
+        //Mohamad
         //test2
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("layout.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
