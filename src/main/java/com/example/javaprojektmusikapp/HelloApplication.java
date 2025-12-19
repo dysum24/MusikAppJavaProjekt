c5fdfcf4c358cce6dd6dd66378fd2d42cd69245d
Mohamadzin <zinmohamad921@gmail.com>
2025-12-19 11:16:14 +0100

    test

--- src/main/java/com/example/javaprojektmusikapp/HelloApplication.java
+++ src/main/java/com/example/javaprojektmusikapp/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException
     {
         // TEST
-        //Testaaaaaaaaa
+        //Testaaaaaaaa
         //test2
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("layout.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
