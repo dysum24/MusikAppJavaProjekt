44e5f273367865deede0ad7d6d52b27651b69985
dysum24 <sumnerdylan@student.dv-schulen.de>
2026-01-03 15:32:38 +0100

    favorites class

--- src/main/java/com/example/javaprojektmusikapp/service/FavoritesService.java
+++ src/main/java/com/example/javaprojektmusikapp/service/FavoritesService.java
@@ -1,4 +1,55 @@
 package com.example.javaprojektmusikapp.service;
 
-public class FavoritesService {
+import com.example.javaprojektmusikapp.model.Song;
+import com.example.javaprojektmusikapp.util.DHL;
+import com.example.javaprojektmusikapp.util.DHLIO;
+import com.example.javaprojektmusikapp.util.DHLNIO;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+
+public class FavoritesService
+{
+    private ArrayList<Song> favorites = new ArrayList<>();
+    private DHL fileHandler = new DHLIO();
+
+    public void addFavorite(Song song)
+    {
+        if(!favorites.contains(song))
+        {
+            favorites.add(song);
+            saveFavorites();
+        }
+    }
+
+    public void removeFavorite(Song song)
+    {
+        favorites.remove(song);
+        saveFavorites();
+    }
+
+    public boolean isFavorite(Song song)
+    {
+        return favorites.contains(song);
+    }
+
+    private void saveFavorites()
+    {
+        BufferedWriter writer = fileHandler.schreiben("./data/", "favorites.csv");
+        if(writer != null)
+        {
+            fileHandler.schreibenDateiCSV(favorites, writer);
+        }
+    }
+
+    public void loadFavorites()
+    {
+        DHLNIO handler = (DHLNIO) fileHandler;
+        BufferedReader reader = handler.lesen("./data/", "favorites.csv");
+        if(reader != null)
+        {
+            favorites = fileHandler.auslesen(reader);
+        }
+    }
 }
